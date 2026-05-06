99f4057a95fa553732110141041ae24483fe8ad2
AMIT <za.mohamedsaleh011213@gmail.com>
2026-05-06 21:48:56 +0300

    add inkwell feature to app button, and create onPress variable.

--- lib/core/theming/colors_manager.dart
+++ lib/core/theming/colors_manager.dart
@@ -3,4 +3,8 @@ import 'package:flutter/material.dart';
 class ColorsManager {
   static const Color primary = Color.fromRGBO(0, 56, 108, 1);
   static const Color white = Colors.white;
+  static const Color backgroundapp = Color.fromRGBO(249, 249, 255, 1);
+  static const Color black = Colors.black;
+  static const Color gray = Colors.grey;
+  static const Color field = Color.fromRGBO(240, 243, 255, 1);
 }
